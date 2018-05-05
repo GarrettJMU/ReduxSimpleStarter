5ea12101c11a7d7e9aad6048265c2df02a3f8a1f
GarrettJMU <garretthughes3@gmail.com>
2018-05-04 21:13:21 -0700

    Getting response from Youtube API

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import SearchBar from './components/search_bar'
+import YTSearch from 'youtube-api-search'
 
-const API_KEY = 'AIzaSyD6HahnkYPjZKEl2kNPlSC0-J7FS5RJupc'
+const API_KEY = 'AIzaSyC--1ALroSsMec6mSb71Sr7aA0DVFI5tZU'
+
+YTSearch({key: API_KEY, term: 'surfboards'}, function(data)  {
+  console.log(data);
+});
 
 const App = () => {
   return <div>
