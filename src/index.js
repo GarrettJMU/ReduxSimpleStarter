269a45237f28edf8a17799997d5ed1289debfc81
GarrettJMU <garretthughes3@gmail.com>
2018-05-04 21:59:38 -0700

    adding in class components

--- src/index.js
+++ src/index.js
@@ -1,18 +1,30 @@
-import React from 'react'
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom'
 import SearchBar from './components/search_bar'
 import YTSearch from 'youtube-api-search'
 
 const API_KEY = 'AIzaSyC--1ALroSsMec6mSb71Sr7aA0DVFI5tZU'
 
-YTSearch({key: API_KEY, term: 'surfboards'}, function(data)  {
-  console.log(data);
-});
 
-const App = () => {
-  return <div>
-            <SearchBar/>
-        </div>;
+
+class App extends Component {
+  constructor(props)  {
+    super(props);
+
+    this.state = { videos: []}
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) =>  {
+      this.setState({ videos })
+    });
+  }
+
+  render()  {
+    return (
+      <div>
+        <SearchBar/>
+      </div>
+    );
+  };
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
